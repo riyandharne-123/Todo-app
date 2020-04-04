abe33a592991f53af67017e92f82154d63902844
riyan <rockydods123@gmail.com>
2020-04-04 17:04:48 +0530

    Updates

---
+++ precache-manifest.d57077fed8ba023b41e9d11697dd1635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bdb89d67b6afb3da392d4231fb3efd",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "8ae12c8960399f624560",
+    "url": "/Todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3bce2679420501223c01",
+    "url": "/Todo-app/static/js/2.2b26d941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.2b26d941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae12c8960399f624560",
+    "url": "/Todo-app/static/js/main.fe121668.chunk.js"
+  },
+  {
+    "revision": "a2ded94475efb83064ec",
+    "url": "/Todo-app/static/js/runtime-main.e0a05951.js"
+  }
+]);
